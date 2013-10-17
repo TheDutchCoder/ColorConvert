cc0c39ab5e264ebbfa45a61e4e22bf71b404f2d4
Reinier Kaper <rp.kaper@gmail.com>
2013-10-17 17:02:49 -0400

    Version 1.1
    Rewrote the whole plugin, supporting hsla and alpha channel retaining.

--- README.md
+++ README.md
@@ -1,32 +1,41 @@
 ColorConvert
 ============
 
-A Sublime Text 2 plugin that converts CSS colors from hex to rgb and back.
+A Sublime Text 2 plugin that converts CSS numerical color values.
+You can convert any hex, rgb(a) or hsl(a) color in the following sequence: hex -> rgba -> hsla -> hex (etc).
 
 ## Installation
-I've submitted the plugin to Package Control by wbond, but until it's implemented, you can just clone this repo in your sublime text 2 packages directory (don't put it in the User sub dir).
+You can easily install the pluing through Will Bond's excellent Package Control (https://sublime.wbond.net/).
+If you want to install this plugin manually for some reason, simply clone this repo into your packages directory (make sure not to put it in the user sub dir).
 
 ## Instructions
-Hit ctrl+shift+c by default (cmd+shift+c on Mac OS) to switch between values.
+1. Select a color declaration (e.g. '#ff0022', 'rgb(120, 0, 12)', or 'hsla(320, 75%, 10%, 0.2)';
+2. Hit ctrl+shift+c by default (cmd+shift+c on Mac OS) to convert to the next type (hex -> rgba -> hsla -> hex -> etc).
 
-    color: #f20645
-    
-    /* Select the hex, either with, or without the hash.
-       It will be converted to this: */
-    
-    color: rgb(242, 6, 69)
-    
-    /* Pressing ctrl+shift+c again will convert to rgba */
-    
-    color: rgba(242, 6, 69, 1)
-    
-    /* Another ctrl+shift+c and you're back at hex */
-    
-    color: #f20645
+## Supported Features
+The following features are supported:
+[x] Convert hex values to rgba
+[x] Convert rgb(a) values to hsla
+[x] Convert hsl(a) values to hex
+[x] Remembers the alpha value of a selection (e.g. when 'rgba(120, 0, 12, 0.2)' is converted to any other value, the '0.2' alpha value will be remembered, even if you convert the value to hex)
 
-Please note that alpha channel values will not be stored and will automatically revert to "1".
+## Planned Features
+[ ] Convert all colors equal to the selected one in the document (milestone 1.2)
 
-## To-do
-- HSL(a) support
-- Retain alpha channel (if possible)
-- Convert all colors in the document with a shortcut
+## Notes
+1. CSS color names (e.g. 'red', 'transparent', etc) are currently not supported (not planning to unless there's a high demand).
+2. Whenever possible, the plugin will output the shorthand hex value (e.g. '#000' instead of '#000000').
+3. All 'rgb' and 'hsl' values will always be written as 'rgba' and 'hsla'. Every browser that supports rgb/hsl also supports rgba/hsla, so opaque colors simple get an alpha of 1.0.
+4. The output for hsl and alpha channels is in floating point, to ensure a little more accuracy.
+
+## Examples
+```css
+color: rgba(128, 0, 12, 0.5);
+```
+
+Will convert to:
+```css
+color: hsla(354.4, 100.0%, 25.1%, 0.5);
+color: #80000b;
+color: rgba(128, 0, 12, 0.5);
+```
--- colorconvert.py
+++ colorconvert.py
@@ -1,98 +1,219 @@
-# colorconvert
-# Converts CSS hex colors to rgb values and rgb(a) to hex.
+# colorconvert (1.1)
+# coding: utf-8
+#
+# Converts CSS colors from hex to rgb(a), from rgb(a) to hsl(a), and from hsl(a)
+# to hex, while preserving alpha channels.
+#
+# More information about color conversions (speifically rgb to hsl and back)
+# can be found here: http://en.wikipedia.org/wiki/HSL_and_HSV#From_HSL
 
 import sublime
 import sublime_plugin
 import re
 
-# Converts CSS colors rgb(a) <-> hex
-#
-# Examples:
-# #000
-# #f0e
-# #f2e094
-# rgb(128, 0, 0)
-# rgb(0, 9, 126)
-# rgba(120, 34, 19, 0.2)
-#
-# To-do:
-#   - color name support
-
 
 class colorconvertCommand(sublime_plugin.TextCommand):
-    """Converts a CSS hex color to rgba, or rgb(a) to hex.
+    """Converts a CSS color from hex to rgba to hsla to hex (etc).
 
     Attributes:
         sublime_plugin.TextCommand: Sublime Text 2 class basis.
 
     """
 
-    def hexToRgb(self, str):
+    # Initlialization.
+    def __init__(self, view):
+        """Initialization of the class.
+
+        Attributes:
+            self: The colorconvertCommand object.
+            view: The current view.
+
+        """
+
+        # Store the view and color channels. The channels are used for
+        # calculating with non-rounded floats.
+        self.view = view
+
+        self.r = 0
+        self.g = 0
+        self.b = 0
+
+        self.h = 0.0
+        self.s = 0.0
+        self.l = 0.0
+
+        self.alpha = 1.0
+
+
+    def hexToRgb(self, r, g, b):
         """Converts a hex value to an rgb value.
 
         Attributes:
             self: The Regionset object.
-            str: The selected string.
+            r:    The value of the red channel   (00 - ff)
+            g:    The value of the green channel (00 - ff)
+            b:    The value of the blue channel  (00 - ff)
 
         """
 
-        # If hex is shorthand, convert to a double value first.
-        if len(str) == 3:
-            val_1 = (int(str[0:1] * 2, 16))
-            val_2 = (int(str[1:2] * 2, 16))
-            val_3 = (int(str[2:3] * 2, 16))
+        r = (int(r, 16))
+        g = (int(g, 16))
+        b = (int(b, 16))
 
-        else:
-            val_1 = (int(str[0:2], 16))
-            val_2 = (int(str[2:4], 16))
-            val_3 = (int(str[4:6], 16))
+        return [r, g, b, self.alpha]
 
-        # Return the preformatted string with the new values.
-        return 'rgb(%d, %d, %d)' % (val_1, val_2, val_3)
 
-    def rgbToRgba(self, rgb_match):
-        """Converts an rgb value to an rgba value.
+    def rgbToHex(self, r, g, b, a):
+        """Converts an rgb(a) value to a hex value.
 
         Attributes:
             self: The Regionset object.
-            rgb_match: The reg exp collection of matches.
+            r:    The value of the red channel   (0 - 255)
+            g:    The value of the green channel (0 - 255)
+            b:    The value of the blue channel  (0 - 255)
+            a:    The value of the alpha channel (0.0 - 1.0)
 
         """
 
-        val_1 = int(rgb_match.group(1), 10)
-        val_2 = int(rgb_match.group(2), 10)
-        val_3 = int(rgb_match.group(3), 10)
+        r = hex(r)[2:].zfill(2)
+        g = hex(g)[2:].zfill(2)
+        b = hex(b)[2:].zfill(2)
 
-        return 'rgba(%d, %d, %d, 1)' % (val_1, val_2, val_3)
+        # Get the alpha channel and store it globally (if present).
+        if a is not None:
+            self.alpha = a
 
-    def rgbaToHex(self, rgb_match):
-        """Converts an rgba value to a hex value.
+        # If a short notation is possible, splice the values.
+        if (r[0:1] == r[1:2]) and (g[0:1] == g[1:2]) and (b[0:1] == b[1:2]):
+            r = r[1:]
+            g = g[1:]
+            b = b[1:]
+
+        return [r, g, b]
+
+
+    def rgbToHsl(self, r, g, b, a):
+        """Converts an rgb(a) value to an hsl(a) value.
 
         Attributes:
             self: The Regionset object.
-            rgb_match: The reg exp collection of matches.
+            r:    The value of the red channel   (0 - 255)
+            g:    The value of the green channel (0 - 255)
+            b:    The value of the blue channel  (0 - 255)
+            a:    The value of the alpha channel (0.0 - 1.0)
 
         """
 
-        # Convert all values to 10-base integers, strip the leading characters,
-        # convert to hex and fill with leading zero's.
-        val_1 = int(rgb_match.group(1), 10)
-        val_2 = int(rgb_match.group(2), 10)
-        val_3 = int(rgb_match.group(3), 10)
+        r = float(r) / 255.0
+        g = float(g) / 255.0
+        b = float(b) / 255.0
 
-        # But first let's check if it's possible to convert to shorthand hex.
-        if (val_1 % 17 == 0) and (val_2 % 17 == 0) and (val_3 % 17 == 0):
-            val_1 = hex(val_1)[2:3]
-            val_2 = hex(val_2)[2:3]
-            val_3 = hex(val_3)[2:3]
+        # Get the alpha channel and store it globally (if present).
+        if a is not None:
+            self.alpha = a
+
+        # Calculate the hsl values.
+        cmax = max(r, g, b)
+        cmin = min(r, g, b)
+
+        delta = cmax - cmin
+
+        # Hue
+        if (cmax == r) and (delta > 0):
+            h = 60 * (((g - b) / delta) % 6.0)
+
+        elif (cmax == g) and (delta > 0):
+            h = 60 * (((b - r) / delta) + 2.0)
+
+        elif (cmax == b) and (delta > 0):
+            h = 60 * (((r - g) / delta) + 4.0)
+
+        elif (delta == 0):
+            h = 0
+
+        # Lightness
+        l = (cmax + cmin) / 2.0
+
+        # Saturation
+        if (delta == 0):
+            s = 0
 
         else:
-            val_1 = hex(val_1)[2:].zfill(2)
-            val_2 = hex(val_2)[2:].zfill(2)
-            val_3 = hex(val_3)[2:].zfill(2)
+            s = (delta / (1 - abs((2 * l) - 1)))
 
-        # Return the preformatted string with the new values.
-        return '#%s%s%s' % (val_1, val_2, val_3)
+        s = s * 100.0
+        l = l * 100.0
+
+        # Store the hsl values globally, so we can use the unrounded versions
+        # to convert them back.
+        self.h = h
+        self.s = s
+        self.l = l
+
+        return [h, s, l, self.alpha]
+
+
+    def hslToRgb(self, h, s, l, a):
+        """Converts an hsl(a) value to an rgb(a) value.
+
+        Attributes:
+            self: The Regionset object.
+            h:    The value of the hue channel        (0.0 - 360.0)
+            s:    The value of the saturation channel (0.0 - 100.0)
+            l:    The value of the lightness channel  (0.0 - 100.0)
+            a:    The value of the alpha channel      (0.0 - 1.0)
+
+        """
+
+        h = float(h)
+        s = float(s) / 100
+        l = float(l) / 100
+
+        # Get the alpha channel and store it globally (if present).
+        if a is not None:
+            self.alpha = a
+
+        # Calculate the rgb values.
+        c = (1 - abs((2 * l) - 1)) * s
+        x = c * (1 - abs(((h / 60.0) % 2) - 1))
+        m = l - (c / 2)
+
+        if (h >= 0) and (h < 60):
+            r = c
+            g = x
+            b = 0
+
+        elif (h >= 60) and (h < 120):
+            r = x
+            g = c
+            b = 0
+
+        elif (h >= 120) and (h < 180):
+            r = 0
+            g = c
+            b = x
+
+        elif (h >= 180) and (h < 240):
+            r = 0
+            g = x
+            b = c
+
+        elif (h >= 240) and (h < 300):
+            r = x
+            g = 0
+            b = c
+
+        elif (h >= 300) and (h < 360):
+            r = c
+            g = 0
+            b = x
+
+        r = int((r + m) * 255.0)
+        g = int((g + m) * 255.0)
+        b = int((b + m) * 255.0)
+
+        return [r, g, b, self.alpha]
+
 
     # Main function.
     def run(self, edit):
@@ -100,7 +221,7 @@ class colorconvertCommand(sublime_plugin.TextCommand):
 
         Attributes:
             self: The Regionset object.
-            edit: The Edit objtec.
+            edit: The Edit object.
 
         """
 
@@ -116,16 +237,20 @@ class colorconvertCommand(sublime_plugin.TextCommand):
             reg_hex = '^[\#]?([\dabcdefABCDEF]){3,6}'
             reg_rgb = ('^rgb[a]?\((\s*\d+\s*),(\s*\d+\s*),(\s*\d+\s*),'
                        '?(\s*(0?.?\d)+\s*)?\)$')
+            reg_hsl = ('^hsl[a]?\((\s*\d+.?\d+\s*),(\s*\d+.?\d+\%\s*),(\s*\d+.?\d+\%\s*),'
+                       '?(\s*(0?.?\d)+\s*)?\)$')
 
             hex_match = re.match(reg_hex, str)
             rgb_match = re.match(reg_rgb, str)
+            hsl_match = re.match(reg_hsl, str)
+
 
             # If a hexadecimal number is found, convert it to an rgb value.
             if hex_match is not None:
 
                 # Expand the selection if we're dealing with hex, but the
-                # hashtag is not part of the selection.
-                if str_len == 3 or str_len == 6:
+                # hashtag is not yet part of the selection.
+                if (str_len == 3) or (str_len == 6):
                     start = sel.begin() - 1
                     end = sel.end()
 
@@ -133,23 +258,73 @@ class colorconvertCommand(sublime_plugin.TextCommand):
                     self.view.sel().add(sublime.Region(start, end))
                     sel = sublime.Region(start, end)
 
+                # If the hashtag is already part of the selection, make sure it
+                # gets stripped.
                 else:
                     str = str[1:]
 
-                # Replace the current selection with the rgb value.
-                self.view.replace(edit, sel, self.hexToRgb(str))
-
-            # If an rgb value is found, convert it to a hexadecimal number.
-            elif rgb_match is not None:
-
-                if rgb_match.group(4) is None:
-
-                    # Replace the current selection with the rgba value.
-                    self.view.replace(edit, sel,
-                                      self.rgbToRgba(rgb_match))
+                if (str_len == 3):
+                    r = str[0:1] * 2
+                    g = str[1:2] * 2
+                    b = str[2:3] * 2
 
                 else:
+                    r = str[0:2]
+                    g = str[2:4]
+                    b = str[4:6]
 
-                    # Replace the current selection with the hex value.
-                    self.view.replace(edit, sel,
-                                      self.rgbaToHex(rgb_match))
+                # Get the rgb(a) values and output them to the current
+                # selection.
+                rgba_vals = self.hexToRgb(r, g, b)
+                output = 'rgba(%d, %d, %d, %s)' % (rgba_vals[0], rgba_vals[1],
+                                                   rgba_vals[2], rgba_vals[3])
+                self.view.replace(edit, sel, output)
+
+
+            # If an rgb(a) value is found, convert it to an hsla value.
+            elif rgb_match is not None:
+
+                # Get all the color channels.
+                r = int(rgb_match.group(1), 10)
+                g = int(rgb_match.group(2), 10)
+                b = int(rgb_match.group(3), 10)
+
+                # If applicable, also get the alpha channel.
+                if (rgb_match.group(4) is not None):
+                    a = float(rgb_match.group(4))
+
+                else:
+                    a = self.alpha
+
+                # Get the hsl(a) values and output them to the current
+                # selection.
+                hsla_vals = self.rgbToHsl(r, g, b, a)
+                output = 'hsla(%.1f, %.1f%%, %.1f%%, %s)' % (hsla_vals[0],
+                                                             hsla_vals[1],
+                                                             hsla_vals[2],
+                                                             hsla_vals[3])
+                self.view.replace(edit, sel, output)
+
+
+            # If an hsl(a) value is found, convert it to a hex value.
+            elif hsl_match is not None:
+
+                # Get all the color channels.
+                h = float(hsl_match.group(1))
+                s = float(hsl_match.group(2).strip('%'))
+                l = float(hsl_match.group(3).strip('%'))
+
+                # If applicable, also get the alpha channel.
+                if (hsl_match.group(4) is not None):
+                    a = float(hsl_match.group(4))
+
+                else:
+                    a = self.alpha
+
+                # Get the rgb(a) values, then the hex values and output them to
+                # the current selection.
+                rgba_vals = self.hslToRgb(h, s, l, a)
+                hex_vals = self.rgbToHex(rgba_vals[0], rgba_vals[1],
+                                         rgba_vals[2], rgba_vals[3])
+                output = '#%s%s%s' % (hex_vals[0], hex_vals[1], hex_vals[2])
+                self.view.replace(edit, sel, output)
